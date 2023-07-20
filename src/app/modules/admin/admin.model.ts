7048e2a3aeb66c8a7fc5aeb2e8e810c02ed7ea7c
parvez13913 <parvezz13913@gmail.com>
2023-07-20 20:27:53 +0600

    [DONE] admin password hashing

--- src/app/modules/admin/admin.model.ts
+++ src/app/modules/admin/admin.model.ts
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { Schema, model } from 'mongoose';
 import { AdminModel, IAdmin } from './admin.interface';
 import { Role } from './admin.constants';
+import bcrypt from 'bcrypt';
+import config from '../../../config';
 
 const adminSchema = new Schema<IAdmin, AdminModel>(
   {
     password: {
       type: String,
       required: true,
+      select: 0,
     },
     role: {
       type: String,
@@ -39,5 +43,15 @@ const adminSchema = new Schema<IAdmin, AdminModel>(
     },
   }
 );
+
+adminSchema.pre('save', async function (next) {
+  const admin = this;
+  admin.password = await bcrypt.hash(
+    admin.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+
+  next();
+});
 
 export const Admin = model<IAdmin, AdminModel>('Admin', adminSchema);
