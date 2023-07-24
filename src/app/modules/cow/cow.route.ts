aead4ccb0dd0adcab1be74fdbcb76553670cb3e3
parvez13913 <parvezz13913@gmail.com>
2023-07-24 11:59:54 +0600

    [DONE] cow authorization

--- src/app/modules/cow/cow.route.ts
+++ src/app/modules/cow/cow.route.ts
@@ -2,21 +2,33 @@ import express from 'express';
 import { CowController } from './cow.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { CowValidation } from './cow.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.SELLER),
   validateRequest(CowValidation.createCowZodSchema),
   CowController.createCow
 );
-router.get('/:id', CowController.getSingleCow);
-router.get('/', CowController.getAllCows);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.SELLER),
+  CowController.getSingleCow
+);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.SELLER),
+  CowController.getAllCows
+);
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.SELLER),
   validateRequest(CowValidation.updatedCowZodSchema),
   CowController.updateCow
 );
-router.delete('/:id', CowController.deleteCow);
+router.delete('/:id', auth(ENUM_USER_ROLE.SELLER), CowController.deleteCow);
 
 export const CowRoutes = router;
--- src/enums/user.ts
+++ src/enums/user.ts
@@ -2,4 +2,6 @@
 export enum ENUM_USER_ROLE {
   ADMIN = 'admin',
   USER = 'user',
+  SELLER = 'seller',
+  BUYER = 'buyer',
 }
